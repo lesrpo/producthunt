272376eb56ceb071b150bfaed190fe8c8221a514
lesrpo <leslierestrepo@gmail.com>
2015-09-01 12:13:47 +0400

    Replace routes to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'products', to: 'products#index', as: :products
-  get 'products/new', to: 'products#new', as: :new_product
-  post '/products', to: 'products#create'
+  # get 'products', to: 'products#index', as: :products
+  # get 'products/new', to: 'products#new', as: :new_product
+  # post '/products', to: 'products#create'
+  # get '/products/:id', to: 'products#show'
+  # get '/products/:id/edit', to: 'products#edit'
+  # patch '/products/:id', to: 'products#update', as: 'product'
+  # delete '/products/:id', to: 'products#destroy'
+  resources :products
+
   get '/welcome', to: 'welcome#index'
-  get '/products/:id', to: 'products#show'
-  get '/products/:id/edit', to: 'products#edit'
-  patch '/products/:id', to: 'products#update', as: 'product'
 end
