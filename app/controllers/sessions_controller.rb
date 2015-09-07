0c49c4b2de87c6c69185e17cc41bcd65af7e07d7
lesrpo <leslierestrepo@gmail.com>
2015-09-07 15:54:01 +0400

    Implement logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,4 +11,9 @@ class SessionsController < ApplicationController
   		render :new
   	end
   end
+
+  def destroy
+  	sign_out
+  	redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-
+  delete 'logout', to: 'sessions#destroy'
   resources :users, only: [:new,:create]
   # get 'products', to: 'products#index', as: :products
   # get 'products/new', to: 'products#new', as: :new_product
