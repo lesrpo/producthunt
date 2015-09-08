2d531d12ede29e55611376d84557305e94e9d8fd
lesrpo <leslierestrepo@gmail.com>
2015-09-08 08:58:59 +0400

    Annotate gem on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(100)
+#  password_digest :string
+#  name            :string(100)
+#  twitter_handle  :string(50)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
 	has_secure_password validations: false
 end
