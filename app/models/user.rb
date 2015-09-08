3db3195ae268bd7c0b97d0d6f8ddab6d3d5be889
lesrpo <leslierestrepo@gmail.com>
2015-09-08 09:19:55 +0400

    Add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,9 @@
 
 class User < ActiveRecord::Base
 	has_secure_password validations: false
+
+	validates :email, uniqueness: true, format: /@/
+	validates :password, presence: true, on: :create
+	validates :password, length: {in: 6..20}, allow_nil: true
+	validates :name, presence:true
 end
